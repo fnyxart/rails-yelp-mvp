2cdd893a0210300ecb688c7bd144f28ef1fac18c
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2021-11-10 12:06:14 +0000

    Add validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
-  # Validations
+  # Restaurant Validations
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :category, presence: true,
+                       inclusion: { in:  %w(chinese italian japanese french belgian) }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,11 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   # Validations
+  validates :rating, presence: true,
+                     inclusion: 0..5,
+                     numericality: { only_integer: true }
+  validates :content, presence: true
+  # helper when your model has associations with other models
+  # and they also need to be validated
+  validates_associated :restaurant
 end
