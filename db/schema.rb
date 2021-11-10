09cc1f1988bbcb0184b1afc0013e9243728d28a3
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2021-11-10 12:18:07 +0000

    Add chef_name migration and seed restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_113146) do
+ActiveRecord::Schema.define(version: 2021_11_10_121649) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_11_10_113146) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "chef_name"
   end
 
   create_table "reviews", force: :cascade do |t|
